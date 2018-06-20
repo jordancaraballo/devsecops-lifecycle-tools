e73a7a229037f656905ec62b9d5f3a889d1c63ee
Jordan A Caraballo Vega <jordan.caraballo@upr.edu>
2018-06-20 13:33:25 -0400

    Inital commit

---
+++ src/ci-test.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ci-test.cpp
+// Author      : Jordan A. Caraballo-Vega
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello People!!!" << endl; // prints !!!Hello World!!!
+	cout << "This is our first commit for this CI Project. This commit will compile succesfully." << endl;
+	return 0;
+}
