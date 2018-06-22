1fdd734296e1c15f07cb5d0d3aec234f8aef7fef
root <root@jira-bamboo.nccs.nasa.gov>
2018-06-22 11:55:05 -0400

    Added syntax error on purpose to check Bamboo

--- src/ci-test.cpp
+++ src/ci-test.cpp
@@ -15,11 +15,17 @@ int sumValues(int,int);
 
 // Main
 int main() {
+
+        // Friendly output
 	cout << "!!!Hello People!!!" << endl; // prints !!!Hello World!!!
 	cout << "This is our first commit for this CI Project. This commit will compile succesfully." << endl;
 
         int sum = sumValues(2,3);
         cout << "The result of the sum is " << sum << ". Ha! Can you guess the values?" << endl; 
+
+        // Adding syntax error to see if build fails over bamboo
+        int huge = {0,0,0,0}
+
 	return 0;
 }
 
